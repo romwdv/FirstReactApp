322eac260f8140a1689b6ba29e3046dccc2cbfad
Romain <romain@mbprom.home>
2026-05-25 22:04:50 +0200

    Update footer lnk

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,9 @@ const Footer = (props) => {
     <footer>
       <h4>
         Made with <span>{props.whith}</span> at <span>{props.at}</span> by{" "}
-        <span>{props.by}</span>
+        <span>
+          <a href="https://github.com/romwdv/FirstReactApp">{props.by}</a>
+        </span>
       </h4>
     </footer>
   );
